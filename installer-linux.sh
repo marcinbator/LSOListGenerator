ce399b243b18d00fe11bde97eb6ac43d97a156fa
Marcin Bator <marcinbator.ofc@gmail.com>
2025-04-18 21:40:25 +0200

    feat: #main installers

--- installer-linux.sh
+++
@@ -1,7 +0,0 @@
-jpackage   \
-  --input target/   \
-  --name LSOListGenerator   \
-  --main-jar LSOListGenerator-1.0.jar   \
-  --main-class pl.bator.lso_list_generator.LSOListGeneratorApplication   \
-  --type deb   \
-  --icon src/main/resources/logo.png
